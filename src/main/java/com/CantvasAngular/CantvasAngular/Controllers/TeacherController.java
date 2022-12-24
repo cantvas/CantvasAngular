0c7e212c91e46aee97562a26602fb9c451642d8b
Mehtab Riar <mehtabriar228@hotmail.com>
2022-12-23 20:27:44 -0800

    added students and teachers to courses

--- src/main/java/com/CantvasAngular/CantvasAngular/Controllers/TeacherController.java
+++ src/main/java/com/CantvasAngular/CantvasAngular/Controllers/TeacherController.java
@@ -1,11 +1,14 @@
 package com.CantvasAngular.CantvasAngular.Controllers;
 
+import com.CantvasAngular.CantvasAngular.Models.Course;
 import com.CantvasAngular.CantvasAngular.Models.Student;
 import com.CantvasAngular.CantvasAngular.Models.Teacher;
+import com.CantvasAngular.CantvasAngular.Repository.CourseRepository;
 import com.CantvasAngular.CantvasAngular.Repository.TeacherRepository;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,9 @@ public class TeacherController {
     @Autowired
     TeacherRepository teacherRepository;
 
+    @Autowired
+    CourseRepository courseRepository;
+
     @GetMapping("/teachers")
     public List<Teacher> getAllTeachers() {
         return teacherRepository.findAll();
@@ -23,7 +29,8 @@ public class TeacherController {
 
     @PostMapping("/teacher")
     public int createTeacher() {
-        Teacher s = new Teacher("sven", "svenlieberman93@gmail.com");
+        Course course = courseRepository.getById(1L);
+        Teacher s = new Teacher("Ian", "Ianlieberman93@gmail.com", course);
         teacherRepository.save(s);
         return Response.SC_OK;
     }
