c8c9d755f02d9746323c8028fb7ce04cbe160a12
Joe Rutkin <rutkinj@gmail.com>
2022-12-28 14:14:11 -0800

    adds gradebook model, nonworking

---
+++ src/main/java/com/CantvasAngular/CantvasAngular/Models/Gradebook.java
@@ -0,0 +1,59 @@
+package com.CantvasAngular.CantvasAngular.Models;
+
+import jakarta.persistence.*;
+
+import java.util.HashMap;
+
+//@Entity
+public class Gradebook {
+//    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Long id;
+
+//    @OneToOne
+    private Student student;
+
+    private Course course;
+
+    private HashMap<Assignment, Integer> gradesMap;
+
+    public Gradebook(){};
+
+//    public Long getId() {
+//        return id;
+//    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public HashMap<Assignment, Integer> getGradesMap() {
+        return gradesMap;
+    }
+
+    public void setGradesMap(HashMap<Assignment, Integer> gradesMap) {
+        this.gradesMap = gradesMap;
+    }
+
+    public void buildGradesMap(){
+        for(Assignment assignment : course.getAssignmentList()){
+            gradesMap.put(assignment, null);
+        }
+    }
+
+    public void updateGrade(Assignment assignment, Integer newGrade){
+        gradesMap.put(assignment, newGrade);
+    }
+
+}
