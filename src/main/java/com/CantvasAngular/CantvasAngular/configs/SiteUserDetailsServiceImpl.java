5607f7a3cfad95ee3decd9534aac8f625e8cbbfa
Mehtab Riar <mehtabriar228@hotmail.com>
2022-12-27 20:36:17 -0800

    implemented security

---
+++ src/main/java/com/CantvasAngular/CantvasAngular/configs/SiteUserDetailsServiceImpl.java
@@ -0,0 +1,59 @@
+package com.CantvasAngular.CantvasAngular.configs;
+
+import com.CantvasAngular.CantvasAngular.Repository.SiteUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+
+@Configuration
+public class SiteUserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    SiteUserRepository siteUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return null;
+            }
+
+            @Override
+            public String getUsername() {
+                return null;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return false;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+    }
+}
