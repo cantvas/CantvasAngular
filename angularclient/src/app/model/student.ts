2c033ad7b03532e3ebc7e8a723c130b8247b879c
Joe Rutkin <rutkinj@gmail.com>
2022-12-29 22:11:32 -0800

    minor styling: assignment detail, edits student diaplay and nav

--- angularclient/src/app/model/student.ts
+++ angularclient/src/app/model/student.ts
@@ -4,9 +4,9 @@ export class Student {
   id: number;
   name: string;
   email: string;
-  course: Course;
+  course: number;
 
-  constructor(id: number, name: string, email: string, course: Course) {
+  constructor(id: number, name: string, email: string, course: number) {
     this.id = id;
     this.name = name;
     this.email = email;
