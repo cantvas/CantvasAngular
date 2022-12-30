2f21b976b1410717e6ac0772d11e0b149fdad2ea
Ben Lieberman <bhlieberman93@gmail.com>
2022-12-29 17:41:30 -0700

    backend logic for course updates

--- src/main/java/com/CantvasAngular/CantvasAngular/Repository/CourseRepository.java
+++ src/main/java/com/CantvasAngular/CantvasAngular/Repository/CourseRepository.java
@@ -2,8 +2,15 @@ package com.CantvasAngular.CantvasAngular.Repository;
 
 import com.CantvasAngular.CantvasAngular.Models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     @Override
     Course getById(Long id);
+
+    @Modifying
+    @Query(value = "update Course c set c.name = :course.name where :id = :courseId", nativeQuery = true)
+    void updateCourse(@Param("course") Course course, @Param("courseId") Long courseId);
 }
